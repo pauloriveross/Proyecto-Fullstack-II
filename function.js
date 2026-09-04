7eeed3068026ad667e66b08b83a5b7974b3681c5
moka_hubs <joaquinfuenzalida727@gmail.com>
2026-09-04 17:25:53 -0400

    ahora funciona los filtros del navbar y se muestran la card de cada productos que corresponde a cada categoria , se añaden mas productos para hacer categoria mas vendida

--- function.js
+++ function.js
@@ -1,4 +1,3 @@
-// 1. Lista de productos
 const productos = [
   { codigo: "TC001",imagen:'tortacuadradachocolate.png', categoria: "Tortas Cuadradas", nombre: "Torta Cuadrada de Chocolate", precio: 45000 , descripcion: "Deliciosa torta de chocolate con capas de ganache y un toque de avellanas personalizable con mensajes especiales"},                                                                                                                                                                                                                            
   { codigo: "TC002",imagen:'tortacuadradafrutas.png', categoria: "Tortas Cuadradas", nombre: "Torta Cuadrada de Frutas", precio: 50000, descripcion : "Una mezcla de frutas frescas y crema chantilly sobre un suave bizcocho de vainilla, ideal para celebraciones."},
@@ -13,10 +12,27 @@ const productos = [
   { codigo: "PT002",imagen:'tartasantiago.png', categoria: "Pastelería Tradicional", nombre: "Tarta de Santiago", precio: 6000 , descripcion : "Tradicional tarta española hecha con almendras, azúcar, y huevos, una delicia para los amantes de los postres clásicos."},
   { codigo: "PG001",imagen:'browniesingluten.png', categoria: "Productos Sin Gluten", nombre: "Brownie Sin Gluten", precio: 4000 , descripcion : "Rico y denso, este brownie es perfecto para quienes necesitan evitar el gluten sin sacrificar el sabor."},
   { codigo: "PG002",imagen:'pansingluten.png', categoria: "Productos Sin Gluten", nombre: "Pan Sin Gluten", precio: 3500 , descripcion : "Suave y esponjoso, ideal para sándwiches o para acompañar cualquier comida."},
-  { codigo: "PV001",imagen:'tortaveganachocolate.png', categoria: "Productos Vegana", nombre: "Torta Vegana de Chocolate", precio: 50000 , descripcion : "Torta de chocolate húmeda y deliciosa, hecha sin productos de origen animal, perfecta para veganos."},
-  { codigo: "PV002",imagen:'galletasveganasavena.png', categoria: "Productos Vegana", nombre: "Galletas Veganas de Avena", precio: 4500 , descripcion : "Crujientes y sabrosas, estas galletas son una excelente opción para un snack saludable y vegano." },
+  { codigo: "PV001",imagen:'tortaveganachocolate.png', categoria: "Productos Veganos", nombre: "Torta Vegana de Chocolate", precio: 50000 , descripcion : "Torta de chocolate húmeda y deliciosa, hecha sin productos de origen animal, perfecta para veganos."},
+  { codigo: "PV002",imagen:'galletasveganasavena.png', categoria: "Productos Veganos", nombre: "Galletas Veganas de Avena", precio: 4500 , descripcion : "Crujientes y sabrosas, estas galletas son una excelente opción para un snack saludable y vegano." },
   { codigo: "TE001",imagen:'tortacumple.png', categoria: "Tortas Especiales", nombre: "Torta Especial de Cumpleaños", precio: 55000 , descripcion : "Diseñada especialmente para celebraciones, personalizable con decoraciones y mensajes únicos."},
-  { codigo: "TE002",imagen:'tortaboda.png', categoria: "Tortas Especiales", nombre: "Torta Especial de Boda", precio: 60000 , descripcion : " Elegante y deliciosa, esta torta está diseñada para ser el centro de atención en cualquier boda."}
+  { codigo: "TE002",imagen:'tortaboda.png', categoria: "Tortas Especiales", nombre: "Torta Especial de Boda", precio: 60000 , descripcion : " Elegante y deliciosa, esta torta está diseñada para ser el centro de atención en cualquier boda."},
+  { codigo: "TC004", imagen: 'tortatrufa.png', categoria: "Tortas Cuadradas", nombre: "Torta Cuadrada de Trufa", precio: 46990, descripcion: "Intensa torta cuadrada con un suave bizcocho de chocolate relleno de abundante trufa artesanal y finas notas de cacao." },
+  { codigo: "TT003", imagen: 'tortacirculartresleches.png', categoria: "Tortas Circulares", nombre: "Torta Circular Tres Leches", precio: 38990, descripcion: "Clásico bizcocho empapado en nuestra mezcla especial de tres leches, cubierto con un delicado merengue italiano." },
+  { codigo: "PI003", imagen: 'suspirolimena.png', categoria: "Postres Individuales", nombre: "Suspiro Limeño", precio: 4800, descripcion: "Tradicional postre envasado en vasito individual, con una suave base de manjar blanco y coronado con merengue al oporto." },
+  { codigo: "PSA003", imagen: 'muffinsinazucar.png', categoria: "Productos Sin Azúcar", nombre: "Muffin Integral de Arándanos Sin Azúcar", precio: 2500, descripcion: "Esponjoso muffin horneado con harina integral y endulzado con tagatosa, repleto de arándanos frescos." },
+  { codigo: "PT003", imagen: 'kuchenframbuesa.png', categoria: "Pastelería Tradicional", nombre: "Kuchen Alemán de Frambuesa", precio: 2990, descripcion: "Tradicional pastel de repostería sureña con una base de masa noble, relleno de jugosas frambuesas y un toque de migas crujientes (streusel)." },
+  { codigo: "PG003", imagen: 'alfajorsingluten.png', categoria: "Productos Sin Gluten", nombre: "Alfajor Artesanal Sin Gluten", precio: 2200, descripcion: "Delicadas tapitas crujientes libres de gluten unidas por un generoso relleno de manjar y sutilmente bañadas en chocolate." },
+  { codigo: "PV003", imagen: 'quequeganachevegano.png', categoria: "Productos Veganos", nombre: "Queque Rústico de Plátano y Nuez Vegano", precio: 3500, descripcion: "Queque húmedo y aromático elaborado 100% con ingredientes vegetales, plátanos maduros seleccionados y trozos de nueces." },
+  { codigo: "TE003", imagen: 'tortaaniversario.png', categoria: "Tortas Especiales", nombre: "Torta de Aniversario Corazón", precio: 58000, descripcion: "Hermosa torta con forma de corazón decorada con detalles florales en crema y un sabor especial a elección para celebrar el amor." },
+  { codigo: "MV001", imagen: "cheesecakefrutosrojos.png", categoria: "Más Vendidos", nombre: "Cheesecake de Frutos Rojos", precio: 42000, descripcion: "Nuestro clásico cheesecake de textura suave y cremosa, coronado con una generosa capa de salsa artesanal de frambuesas y moras frescas." },
+  { codigo: "MV002", imagen: "kuchenmanzana.png", categoria: "Más Vendidos", nombre: "Kuchen de Manzana Crujiente", precio: 28900, descripcion: "Receta tradicional de la zona sur, con finas láminas de manzana acarameladas sobre una masa suave y cubierta de migas crujientes de manteca." },
+  { codigo: "MV003", imagen: "volcan.png", categoria: "Más Vendidos", nombre: "Volcán de Chocolate Belga", precio: 6500, descripcion: "Delicioso pastelito individual de chocolate con un núcleo fundente y cálido que se desborda al primer corte. Ideal para acompañar con helado." },
+  { codigo: "MV004", imagen: "tortamanjarlucuma.png", categoria: "Más Vendidos", nombre: "Torta Hojarasca Manjar Lúcuma", precio: 44000, descripcion: "Una de las favoritas de la casa: capas ultrafinas y crujientes de hojarasca rellenas de manjar de leche condensada y crema ligera de lúcuma." },
+  { codigo: "MV005", imagen: "cajaalfajor.png", categoria: "Más Vendidos", nombre: "Caja de 6 Alfajores Artesanales", precio: 7500, descripcion: "Exquisitos alfajores rellenos con abundante manjar casero, bañados en cobertura de chocolate bitter y con bordes de coco tostado." },
+  { codigo: "MV006", imagen: "pielimon.png", categoria: "Más Vendidos", nombre: "Pie de Limón Con Merengue", precio: 26000, descripcion: "Crocante masa de tarta con una crema de limón fresca y equilibrada, coronada con copitos de merengue italiano ligeramente dorado." },
+  { codigo: "MV007", imagen: "cinnamon.png", categoria: "Más Vendidos", nombre: "Cinnamon Rolls Glaseados (Pack x4)", precio: 6990, descripcion: "Esponjosos rollitos de canela recién horneados, cubiertos con un dulce y suave glaseado de queso crema que se derrite en la boca." },
+  { codigo: "MV008", imagen: "redvelvet.png", categoria: "Más Vendidos", nombre: "Torta Red Velvet Clásica", precio: 49000, descripcion: "Elegante bizcocho rojo de cacao suave con sutiles notas de vainilla, intercalado y cubierto con una exquisita crema de queso crema (frosting)." }
+
 ];
 
 function crearCard(producto) {
@@ -46,6 +62,7 @@ function crearCard(producto) {
 
 function renderizarProductos(lista) {
   const contenedor = document.getElementById('productos');
+  contenedor.innerHTML = "";
 
   lista.forEach(producto => {
     const card = crearCard(producto);
@@ -54,7 +71,18 @@ function renderizarProductos(lista) {
 }
 
 
+function filtrar(categoria) {
 
+  const titulo = document.getElementById("titulo-catalogo");
+  if (categoria === 'Todos Los Productos') {
+    titulo.innerText="Todos Nuestros Productos"
+    renderizarProductos(productos)
+  }else{
+    titulo.innerText =`${categoria}`
+    const filtro = productos.filter(p=>p.categoria === categoria)
+    renderizarProductos(filtro)
+  }
+}
 
 
 renderizarProductos(productos);
